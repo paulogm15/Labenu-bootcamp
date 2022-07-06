ba3c44f904c326b0cbac7dff727190b8afcfc9d9
paulo Sol <paulogmendes.15@gmail.com>
2022-07-05 23:04:35 -0300

    pokeCard ainda nao funcional

---
+++ modulo3/use-effect/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./styles.css";
+import axios from "axios";
+import PokeCard from "./components/PokeCard/PokeCard";
+
+const App = () => {
+  return (
+    <PokeCard />
+  )
+}
+export default App;
