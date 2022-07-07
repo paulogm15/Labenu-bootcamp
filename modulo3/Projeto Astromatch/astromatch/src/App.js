3eb95839283db879a2aaf725262e779ed187a0ec
paulo Sol <paulogmendes.15@gmail.com>
2022-07-06 23:23:18 -0300

    astromatch work in progress

---
+++ modulo3/Projeto Astromatch/astromatch/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// import styled from 'styled-components';
+
+
+
+const App = () => {
+
+  return (
+    <div class='App'>
+      <h1>AstroMatch</h1>
+      <p>Mais um app de relacionamentos</p>
+    </div>
+  );
+}
+
+export default App;
