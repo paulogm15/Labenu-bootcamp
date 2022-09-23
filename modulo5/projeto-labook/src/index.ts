bd50e3cff45133fef10603a2965823c14387f134
Paulo G M Santos <31298118+paulogm15@users.noreply.github.com>
2022-09-23 20:13:17 -0300

    arquivos base

---
+++ modulo5/projeto-labook/src/index.ts
@@ -0,0 +1,14 @@
+import express, { Express } from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+
+const app: Express = express();
+
+app.use(express.json());
+app.use(cors());
+
+
+app.listen(process.env.PORT || 3003, () => {
+    console.log(`Servidor rodando na porta ${process.env.PORT || 3003}`)
+})
+
